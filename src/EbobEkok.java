5a9dfbb5b89cb161c18e9cf3bec6de5affb2a3c1
Murat Karaca <murat.github@hotmail.com>
2022-01-05 15:18:11 +0300

    Pratik - EBOB/EKOK Bulan Program

---
+++ src/EbobEkok.java
@@ -0,0 +1,32 @@
+import java.util.Optional;
+import java.util.Scanner;
+
+public class EbobEkok {
+    public static void main(String[] args) {
+        Scanner input = new Scanner( System.in );
+
+        System.out.print("Birinci Sayıyı Girin: ");
+        int n1 = input.nextInt();
+
+        System.out.print("İkinci Sayıyı Girin: ");
+        int n2 = input.nextInt();
+
+        int ebob = getEbob( n1 , n2 );
+        int ekok = n1 * n2 / ebob;
+        System.out.println("Sayıların Ebobu : " + ebob + "\nSayıların Ekoku : " + ekok);
+    }
+
+    public static int getEbob(int n1, int n2){
+        int smallerNumber = n1 > n2 ? n2 : n1;
+        int result = 1;
+        while(smallerNumber > 1){
+            if (n1 % smallerNumber == 0 && n2 % smallerNumber == 0){
+                result = smallerNumber;
+                break;
+            }
+            smallerNumber--;
+        }
+        return result;
+    }
+
+}
